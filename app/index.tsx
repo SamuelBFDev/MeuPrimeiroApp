a4ca9dea5be1ed38a2668174a3983fc3ad81e7b2
samuelbuenofran <samuel.buenofran@gmail>
2024-03-06 20:58:59 -0300

    Added Gluestack dependencies

--- app/index.tsx
+++ app/index.tsx
@@ -1,32 +1,66 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import StyledInput from "../components/StyledInput";
 import StyledButton from "../components/StyledButton";
 import { router } from "expo-router";
+import {
+  GluestackUIProvider,
+  Card,
+  Heading,
+  Text,
+  InputField,
+  Input,
+} from "@gluestack-ui/themed";
+import { config } from "@gluestack-ui/config";
 import DisplayAnImage from "../components/StyledImage";
 
 export default function Login() {
   const handleLogin = () => {
-    router.push("/home");
+    router.push("home");
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-      <DisplayAnImage />
+    <GluestackUIProvider config={config}>
+      <View style={styles.container}>
+        <Card size="md" variant="elevated" m="$3">
+          <Heading mb="$1" size="md">
+            Quick Start
+          </Heading>
+          <Text size="sm">Start building your next project in minutes</Text>
+        </Card>
+
+        <Text size="sm"> Login screen</Text>
+
+        <Input
+          variant="outline"
+          size="md"
+          isDisabled={false}
+          isInvalid={false}
+          isReadOnly={false}
+        >
+          <InputField placeholder="Enter Text here" />
+        </Input>
+
+        <View style={styles.imageContainer}>
+          <DisplayAnImage />
+        </View>
+        <Text>Tela de login</Text>
+        <StyledInput
+          onChangeText={(texto) => console.log("Email:", texto)}
+          placeholder="Digite o seu email: "
+        />
+        <StyledInput
+          onChangeText={(texto) => console.log("Senha", texto)}
+          placeholder="Digite a sua senha: "
+        />
+        <StyledButton
+          title="Acessar o app"
+          OnPress={handleLogin}
+          color="blue"
+        />
+        <StatusBar style="auto" />
       </View>
-      <Text>Tela de login</Text>
-      <StyledInput
-        onChangeText={(texto) => console.log("Email:", texto)}
-        placeholder="Digite o seu email: "
-      />
-      <StyledInput
-        onChangeText={(texto) => console.log("Senha", texto)}
-        placeholder="Digite a sua senha: "
-      />
-      <StyledButton title="Acessar o app" OnPress={handleLogin} color="blue" />
-      <StatusBar style="auto" />
-    </View>
+    </GluestackUIProvider>
   );
 }
 
@@ -52,5 +86,5 @@ const styles = StyleSheet.create({
     alignItems: "flex-start",
     alignSelf: "auto",
     backgroundColor: "white",
-    },
+  },
 });
